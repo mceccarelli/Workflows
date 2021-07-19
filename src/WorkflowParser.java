c533d5a9030d0387482da19d0fa885295e26747e
amohan <amohan@amohanmacpro.local>
2021-07-19 14:15:33 -0400

    added pworkflow load and display code

--- src/WorkflowParser.java
+++ src/WorkflowParser.java
@@ -49,11 +49,49 @@ public class WorkflowParser {
 		}
 		
 	}
+	public static Map<String, Map<String, String>> loadPWorkflows(String pSpecFile){
+		Map<String, Map<String, String>> pwfs = new HashMap<String, Map<String, String>>();
+		try {
+      		File file = new File(pSpecFile);
+		    Scanner scan = new Scanner(file);
+		    while (scan.hasNextLine()) {
+			    String[] workflow = scan.nextLine().split(" = ");
+			    if (workflow.length == 2){
+			    	String workflowName = workflow[0];
+			    	String[] workflowPorts = workflow[1].split("]");	
+			    	if (workflowPorts.length == 2){
+			    		String inputPorts = workflowPorts[0].substring(workflowPorts[0].indexOf("[IP: ") + 4, workflowPorts[0].length());
+			    		String outputPorts = workflowPorts[1].substring(workflowPorts[1].indexOf("[OP: ") + 4, workflowPorts[1].length());
+			    		Map<String, String> ports = new HashMap<String, String>();
+			    		ports.put(inputPorts, outputPorts);
+			    		pwfs.put(workflowName, ports);
+			    	}
+			    }
+		    }
+		    scan.close();
+    	} catch (FileNotFoundException e) {
+      		System.out.println("An error occurred.");
+      		e.printStackTrace();
+    	}
+    	return pwfs;
+	}
+	public static void displayPWorkflows(Map<String, Map<String, String>> pwfs){
+		 for (Map.Entry<String, Map<String, String>> pwf : pwfs.entrySet()) {
+    	 	String workflowName = pwf.getKey();
+    	 	for (Map.Entry<String, String> port : pwf.getValue().entrySet()) {
+    	 		String inputPorts = port.getKey();
+    	 		String outputPorts = port.getValue();
+    	 		System.out.println(workflowName + "\t" + inputPorts + "\t" + outputPorts);
+    	 	}
+		 }
+	}
 	public static void main(String[] args) {
 		String matrixFile = "../data/wa/matrix.txt";
+		String pSpecFile = "../data/primitive.spec";
 		int[][] matrix = buildMatrix(matrixFile);
 		loadMatrix(matrixFile, matrix);
 		displayMatrix(matrix);
+		displayPWorkflows(loadPWorkflows(pSpecFile));
 
     
    	}
