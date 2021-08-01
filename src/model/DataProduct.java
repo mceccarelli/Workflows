313fdf40fd0a466c2f091c636db6b80d45ff99e2
Michael Ceccarelli <ceccarellim@allegheny.edu>
2021-08-01 16:27:45 -0400

    fixed "dp" display

--- src/model/DataProduct.java
+++ src/model/DataProduct.java
@@ -7,7 +7,7 @@ public class DataProduct extends Port {
 
   // Constructor for the class
   public DataProduct(int number, String value, String type) {
-    super('i', number, type);
+    super('d', number, type);
     this.value = value;
   }
 
@@ -21,40 +21,3 @@ public class DataProduct extends Port {
     return value;
   }
 }
-
-/*
-public class DataProduct<T> {
-  // The parts of a DataProduct
-  private char id;
-  private int number;
-  private T value;
-  private String type;
-
-  // Constructor for the class
-  public DataProduct(char id, int number, T value, String type) {
-    this.id = id;
-    this.number = number;
-    this.value = value;
-    this.type = type;
-  }
-
-  // toString overriding
-  public String toString() {
-    return id + ": " + type + " = " + value;
-  }
-
-  // Getter methods for the variables
-  public String getID() {
-    return (String)(id + "p" + number);
-  }
-  public int getNumber() {
-    return number;
-  }
-  public T getValue() {
-    return value;
-  }
-  public String getType() {
-    return type;
-  }
-}
-*/
